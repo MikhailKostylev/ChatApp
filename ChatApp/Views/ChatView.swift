f85cb1c73c80bd6fc7d8e447e4bea67f9a471315
Mikhail Kostylev <godhunt.50cal@gmail.com>
2022-07-20 15:37:21 +0300

    Add textField with send button to chatView

--- ChatApp/Views/ChatView.swift
+++ ChatApp/Views/ChatView.swift
@@ -8,18 +8,35 @@
 import SwiftUI
 
 struct ChatView: View {
+    
+    @State var message: String = ""
+    
     var body: some View {
-        ScrollView(.vertical) {
-            ChatRow(type: .sent)
-                .padding(3)
-            ChatRow(type: .received)
-                .padding(3)
+        VStack {
+            ScrollView(.vertical) {
+                ChatRow(type: .sent)
+                    .padding(3)
+                ChatRow(type: .received)
+                    .padding(3)
+            }
+            
+            HStack {
+                TextField("Message...", text: $message)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(8)
+                
+                SendButton(text: $message )
+            }
+            .padding()
         }
+        .navigationTitle("Bob")
     }
 }
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
         ChatView()
+            .preferredColorScheme(.dark)
     }
 }
--- ChatApp/Views/SendButton.swift
+++ ChatApp/Views/SendButton.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct SendButton: View {
+    
+    @Binding var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            self.sendMessage()
+        } label: {
+            Image(systemName: "paperplane")
+                .aspectRatio(contentMode: .fit)
+                .font(.system(size: 30))
+                .frame(width: 55, height: 55)
+                .foregroundColor(Color.white)
+                .background(Color.blue)
+                .clipShape(Circle())
+        }
+    }
+    
+    private func sendMessage() {
+        guard !text.isEmpty else {
+            return
+        }
     }
 }
 
